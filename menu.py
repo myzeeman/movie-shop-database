517f72f96af70917c27e6d685497c694ad375006
myzeeman <80385747+myzeeman@users.noreply.github.com>
2022-09-01 10:08:48 +1200

    Update menu.py
    added backend to menu file

--- menu.py
+++ menu.py
@@ -1,13 +1,38 @@
 import easygui as gui
 
 def menu():
+
+    '''menu function for user to choose what to do to the database'''
+
+    #constant variables for gui button box:
     options = ['Show Table', 'Print Table', 'New Movie', 'Change Movie',
     'Delete Movie', 'Search Movies', 'Exit']
     msg = 'Please choose an option'
     title = 'Menu'
+    #gui button box for main menu gui:
     user_input = gui.buttonbox(msg, title, options)
+    #print(user_input) print statement for testing
 
+    #runs chosen function:
+    if user_input == options[0]:
+        show_table()
 
+    elif user_input == options[1]:
+        print_table()
 
+    elif user_input == options[2]:
+        new_movie()
+
+    elif user_input == options[3]:
+        change_movie()
+
+    elif user_input == options[4]:
+        delete_movie()
+
+    elif user_input == options[5]:
+        search_movies()
+    
+    else:
+        menu()
 if __name__ == '__main__':
     menu()
