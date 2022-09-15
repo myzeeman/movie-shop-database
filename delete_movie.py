c6a955ba5e8979d23baaeb85ab39bff28a100fd5
myzeeman <80385747+myzeeman@users.noreply.github.com>
2022-09-15 14:20:55 +1200

    Update delete_movie.py
    added choice box and printed users choice

--- delete_movie.py
+++ delete_movie.py
@@ -7,7 +7,11 @@ def delete():
     c.execute('SELECT * FROM movies')
     movies = c.fetchall()
     movie_names = [i[0] for i in movies]
-    print(movie_names)
+
+    message = '''Please choose an item to delete or click 'cancel' to exit'''
+    title = 'Delete menu'
+    movie_choice = gui.choicebox(msg = message, title = title, choices = movie_names)
+    print(movie_choice)
 
 if '__main__' == __name__:
     delete()
