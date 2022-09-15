fb664f8634a20cb5d7852d824cce007cffbc961e
myzeeman <80385747+myzeeman@users.noreply.github.com>
2022-09-15 22:02:31 +1200

    Update search_function1.py
    documentation

--- search_function1.py
+++ search_function1.py
@@ -2,17 +2,28 @@ import sqlite3
 import easygui as gui
 
 def search_movies():
+    '''Asks user to search for a movie and displays result'''
+
+    #connecting to database
     conn = sqlite3.connect('movies.db')
     c = conn.cursor()
 
-
-    message = 'Please enter a movie name to search for'
+    #asks for useer to search for a movie
+    message = ('Please enter a movie name to search for \n'+
+              'or click cancel to exit')
     search = gui.enterbox(message)
 
+    #if input is a string and is longer than 0 characters then checks for movie in db
     if (type(search) == str) and (len(search) > 0):
         c.execute('''SELECT * FROM movies WHERE name = ?''', [search])
         movie = c.fetchall()
-        print(movie)
+
+        if len(movie) > 0:
+            print(movie)
+
+        else:
+            print('Movie not found')
+    #else then exits back to menu
 
 if __name__ == '__main__':
     search_movies()
